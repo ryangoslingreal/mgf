d8cb9129998a177f13412898178425442a19002d
ryangoslingreal <alexander.sokolovich05@gmail.com>
2022-11-23 23:07:41 +0000

    tweaks

--- Assets/Scripts/PrimaryWeapon.cs
+++ Assets/Scripts/PrimaryWeapon.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class PrimaryWeapon : MonoBehaviour
 {
-    
+	public GameObject bulletPrefab;
+	public GameObject primaryMuzzle;
+
+	public float fireRate = 600f; // 300rpm = trigger pull every 0.1s.
+	public float muzzleVelocity = 35f; // in m/s.
+
+	public const int magMax = 30;
+	public int mag = 30;
+
+	bool canShoot = true;
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Mouse0) && mag > 0 && canShoot)
+		{
+			StartCoroutine(ShootingDelay());
+			GameObject bullet = Instantiate(bulletPrefab, primaryMuzzle.transform.position, transform.rotation); // create bullet.
+			bullet.SendMessage("SetVelocity", muzzleVelocity); // set bullet's velocity.
+		}
+	}
+
+	IEnumerator ShootingDelay()
+	{
+		canShoot = false;
+		float timeBeforeShooting = (float)60 / (float)fireRate; // calculate time before firing.
+		yield return new WaitForSeconds((float)timeBeforeShooting); // wait.
+		canShoot = true;
+	}
 }
