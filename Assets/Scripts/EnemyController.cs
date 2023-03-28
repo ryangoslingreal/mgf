9f511e8e2478287772a09aee63ea4c08dc46e64c
ryangoslingreal <alexander.sokolovich05@gmail.com>
2023-03-28 17:18:19 +0100

    fghjkl;

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Design.Serialization;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 
@@ -17,12 +18,17 @@ public class EnemyController : MonoBehaviour
 	[HideInInspector]
 	public GameObject player;
 	public bool playerDetected;
+
 	public Transform target;
+	float speed = 1f;
+	Vector3[] path;
+	int targetIndex;
 
 	void Start()
     {
         audioSource = GetComponent<AudioSource>();
-    }
+		PathRequestManager.RequestPath(transform.position, target.position, OnPathFound); // request path.
+	}
 
 	private void OnTriggerEnter(Collider other) // check for collision with bullet.
 	{
@@ -36,5 +42,40 @@ public class EnemyController : MonoBehaviour
                 Destroy(this.gameObject); // die if health < 0.
 			}
         }
+	}
+
+	public void OnPathFound(Vector3[] newPath, bool pathSuccessful) // if path has been found.
+	{
+		if (pathSuccessful)
+		{
+			path = newPath;
+
+			//restart FollowPath coroutine.
+			StopCoroutine("FollowPath");
+			StartCoroutine("FollowPath");
+		}
+	}
+
+	IEnumerator FollowPath()
+	{
+		Vector3 currentWaypoint = path[0]; // set initial waypoint as first waypoint in path array.
+
+		while (true)
+		{
+			if (transform.position == currentWaypoint) // waypoint reached.
+			{
+				targetIndex++; // next waypoint in array.
+
+				if (targetIndex >= path.Length) // prevent index out of bounds error.
+				{
+					yield break;
+				}
+
+				currentWaypoint = path[targetIndex]; // set next waypoint.
+			}
+
+			transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed); // move to waypoint.
+			yield return null;
+		}
 	}
 }
